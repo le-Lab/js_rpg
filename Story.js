afede3a92442eb31b8a54f63dd8518437525a8f6
bu7ch <dutchr@live.fr>
2018-07-29 21:36:26 +0200

    fix format

--- Story.js
+++ Story.js
@@ -1,11 +1,13 @@
 let name = prompt("Quel est le nom de ce personnage ?");
 let gender = prompt("Quel est le genre de ce personnage ? ");
 let race = prompt("Quel est sa race ? (Humain, Elfe, Nain, demi-homme)");
-let characterRole = prompt("Quelle est la classe de ce personnage? (Guerrier, Ranger, Voleur)");
+let characterRole = prompt(
+  "Quelle est la classe de ce personnage? (Guerrier, Ranger, Voleur)"
+);
 
 const mainHero = new Hero(name, 10, gender, race, characterRole);
 
-checkRace(mainHero, mainHero.race)
-checkRace(mainHero, mainHero.characterRole)
+checkRace(mainHero, mainHero.race);
+checkRace(mainHero, mainHero.characterRole);
 
 console.log(mainHero);
